c988acda98a82ced5d3e458eaf56f93c8e860474
PLSGOTOASS <koteykaarisha@mail.ru>
2025-01-03 15:28:20 +0300

    init

---
+++ src/main/java/org/ciaochat/CiaoChatApplication.java
@@ -0,0 +1,13 @@
+package org.ciaochat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiaoChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiaoChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ciaochat/CiaoChatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ciaochat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiaoChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
